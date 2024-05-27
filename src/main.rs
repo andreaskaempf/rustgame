fc9bef967f439bf49b7bc8b268fc6f12dd79b71c
Andreas Kaempf <andreas.kaempf@soundience.com>
2024-05-27 22:37:42 +0300

    Movement of player on screen (just vertical, p. 62)

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,152 @@
 use bracket_lib::prelude::*;
 
+// Game modes
+enum GameMode {
+    Menu,
+    Playing,
+    End,
+}
+
+// Constants
+//const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
+const FRAME_DURATION: f32 = 75.0;
+
+// Current state of dragon
+struct Player {
+    x: i32,
+    y: i32,
+    velocity: f32,
+}
+
+impl Player {
+
+    // Constructor for player
+    fn new(x: i32, y: i32) -> Self {
+        Player {
+            x,
+            y,
+            velocity: 0.0,
+        }
+    }
+
+    // Render player as yellow '@'
+    fn render(&mut self, ctx: &mut BTerm) {
+        ctx.set(
+            0,
+            self.y,
+            YELLOW,
+            BLACK,
+            to_cp437('@')
+        );
+    }
+
+    // Update player
+    fn gravity_and_move(&mut self) {
+        if self.velocity < 2.0 {
+            self.velocity += 0.2;
+        }
+        self.y += self.velocity as i32;
+        self.x += 1;
+        if self.y < 0 {
+            self.y = 0;
+        }
+    }
+
+    // Flap the wings
+    fn flap(&mut self) {
+        self.velocity = -2.0;
+    }
+}
+
 // Game state
-struct State {}
+struct State {
+    player: Player,
+    frame_time: f32,
+    mode: GameMode,
+}
+
+impl State {
+
+    // Constructor for state
+    fn new() -> Self {
+        State {
+            player: Player::new(5, 25),
+            frame_time: 0.0,
+            mode: GameMode::Menu,
+        }
+    }
+
+    // Play the game
+    fn play(&mut self, ctx: &mut BTerm) {
+        ctx.cls_bg(NAVY);
+        self.frame_time += ctx.frame_time_ms;
+        if self.frame_time > FRAME_DURATION {
+            self.frame_time = 0.0;
+            self.player.gravity_and_move();
+        }
+        if let Some(VirtualKeyCode::Space) = ctx.key {
+            self.player.flap();
+        }
+        self.player.render(ctx);
+        ctx.print(0, 0, "Press SPACE to flap.");
+        if self.player.y > SCREEN_HEIGHT {
+            self.mode = GameMode::End;
+        }
+    }
+
+    // Restart game 
+    fn restart(&mut self) {
+        self.player = Player::new(5, 25);
+        self.frame_time = 0.0;
+        self.mode = GameMode::Playing;
+    }
+
+    // Main menu
+    fn main_menu(&mut self, ctx: &mut BTerm) {
+
+        // Show menu
+        ctx.cls();
+        ctx.print_centered(5, "Welcome to Flappy Dragon");
+        ctx.print_centered(8, "(P) Play Game");
+        ctx.print_centered(9, "(Q) Quit Game");
+
+        // Process key
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
+    }
+
+    // Game over
+    fn dead(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print_centered(5, "You are dead!");
+        ctx.print_centered(8, "Press (P) to play again");
+        ctx.print_centered(9, "Press (Q) to quit");
+
+        // Process key
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
+    }
+}
 
 // Trait for game state
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
-        ctx.cls();
-        ctx.print(1, 1, "Hello, Bracket Terminal!");
+        match self.mode {
+            GameMode::Menu => self.main_menu(ctx),
+            GameMode::End => self.dead(ctx),
+            GameMode::Playing => self.play(ctx),
+        }
     }
 }
 
@@ -20,5 +159,5 @@ fn main() -> BError {
         .build()?;  // pass errors to parent function
 
     // Start executing the game loop
-    main_loop(context, State {})  // no semicolon, returns result
+    main_loop(context, State::new())  // no semicolon, returns result
 }
