c302e3d1dbd09ae6bd521b4a60256c7ccbf9e489
Andreas Kaempf <andreas.kaempf@soundience.com>
2024-05-27 22:07:19 +0300

    Initialize and start game windows (p. 53)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
+use bracket_lib::prelude::*;
+
+// Game state
+struct State {}
+
+// Trait for game state
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print(1, 1, "Hello, Bracket Terminal!");
+    }
+}
+
+fn main() -> BError {
     println!("Hello, world!");
+
+    // Create Bracket Terminal context
+    let context = BTermBuilder::simple80x50()
+        .with_title("Flappy Dragon")
+        .build()?;  // pass errors to parent function
+
+    // Start executing the game loop
+    main_loop(context, State {})  // no semicolon, returns result
 }
